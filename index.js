bd53fc282faa35e13dc6323adbe2a243d629af16
Osokoya Fiyinfoluwa <osokoyafiyinfoluwa@gmail.com>
2025-10-16 13:32:57 +0100

    Updated

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const app = express();
 //This creates a server running on our local host
 const port = 3000;
 
+app.get("/",(req,res) =>{
+    res.send(
+        "<h1>Hello!</h1>",
+    )
+})
+
+app.get("/about", (req,res)=>{
+    res.send(
+        "<h1>About Us</h1><p>This is a quick paragraph about me</p>"
+    )
+})
 
 app.listen(port, ()=>{
     console.log(`Server is running on Port: ${port}`);
